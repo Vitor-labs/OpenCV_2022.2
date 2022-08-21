53455e22117017631287367800029df87d2183e4
Vitor Hugo <v02hx10@gmail.com>
2022-08-21 17:45:01 -0300

    MOD: resize

--- rescale.py
+++ rescale.py
@@ -1,9 +1,7 @@
 import cv2 as cv
 
-# resizing an image using cv.resize()
 
-
-def rescale_imgs(frame, scale_factor):
+def rescale_frames(frame, scale_factor):
     width = int(frame.shape[1] * scale_factor)
     height = int(frame.shape[0] * scale_factor)
     dim = (width, height)
@@ -12,41 +10,11 @@ def rescale_imgs(frame, scale_factor):
     return resized
 
 
-def rescale_video(video_path, scale_factor):
-    # read video
-    cap = cv.VideoCapture(video_path)
-    # get video properties
-    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-    # get video fps
-    fps = cap.get(cv.CAP_PROP_FPS)
-    # get video length
-    length = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-    # get video codec
-    codec = cap.get(cv.CAP_PROP_FOURCC)
-    # create video writer
-    fourcc = cv.VideoWriter_fourcc(*'XVID')
-    out = cv.VideoWriter('data/videos/v2.mp4', fourcc, fps, (width, height))
-    # rescale video
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        resized = rescale_imgs(frame, scale_factor)
-        out.write(resized)
-        cv.imshow('frame', resized)
-        if cv.waitKey(1) == ord('q'):
-            break
-    cap.release()
-    out.release()
-    cv.destroyAllWindows()
-
-
-def main():
+def show_rescaled_imgs():
     # read image
     frame = cv.imread('data/images/th1.webp')
     # rescale image
-    resized = rescale_imgs(frame, 2.5)
+    resized = rescale_frames(frame, 0.5)
     # show image
     cv.imshow('frame', resized)
     # wait for key press
@@ -55,5 +23,25 @@ def main():
     cv.destroyAllWindows()
 
 
+def show_rescaled_video():
+    # read video
+    cap = cv.VideoCapture('data/videos/v2.mp4')
+    # rescale video
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        resized = rescale_frames(frame, 0.5)
+        cv.imshow('frame', resized)
+
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
 if __name__ == '__main__':
-    main()
+    #    show_rescaled_imgs()
+    show_rescaled_video()
