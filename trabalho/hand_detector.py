886b461adc4a6fd01c622cc4ce8270c050e8a9be
Vitor Hugo <v02hx10@gmail.com>
2022-12-09 23:45:38 -0300

    MOD: find_position

--- trabalho/hand_detector.py
+++ trabalho/hand_detector.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import mediapipe as mp
 
 
@@ -14,6 +15,8 @@ class HandDetector():
             self.static_image_mode,
             self.max_num_hands)
         self.mp_draw = mp.solutions.drawing_utils
+
+        self.tip_idx = [4,8,12,16,20]
 
 
     def detect_hands(self, frame, draw_landmarks=True):
@@ -33,19 +36,62 @@ class HandDetector():
         """
         returns a list with the coordinates of all landmarks in the detected frame
         """
-        landmarks = []
+        x_list = []
+        y_list = []
+        bbox = []
+        self.landmarks = []
         if self.results.multi_hand_landmarks:
             choice = self.results.multi_hand_landmarks[landmark_number]
             for idx, landmark in enumerate(choice.landmark):
                 h, w, _ = frame.shape
                 cx, cy = int(landmark.x*w), int(landmark.y*h)
-                landmarks.append([idx, cx, cy])
+                x_list.append(cx)
+                y_list.append(cy)
+                self.landmarks.append([idx, cx, cy])
                 if draw_landmark:
                     _, x, y = landmarks[landmark_number]
                     cv2.circle(frame, (x, y), 20, (0,255,255), cv2.FILLED)
-        return landmarks
+            
+            xmin, xmax = min(x_list), max(x_list)
+            ymin, ymax = min(y_list), max(y_list)
+            bbox = xmin, xmax, ymin, ymax
 
+            if draw_landmark:
+                cv2. rectangle(frame, (xmin - 20, ymin - 20), 
+                              (xmax + 20, ymax +20), (0,255,0), 2)
 
+        return self.landmarks, bbox
+
+    def find_distance(self, point1, point2, frame, draw=True, radius=15, tickness=3):
+        x1, y1 = self.landmarks[point1][1:]
+        x2, y2 = self.landmarks[point2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(frame, (x1, y1), (x2, y2), (0,255,255), tickness)
+            cv2.circle(frame, (x1, y1), radius, (0,255,255), cv2.FILLED)
+            cv2.circle(frame, (x2, y2), radius, (0,255,255), cv2.FILLED)
+            cv2.circle(frame, (cx, cy), radius, (0,255,0), cv2.FILLED)
+        
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, frame, [x1, y1, x2, y2, cx, cy]
+
+    def fingers_up(self, ):
+        fingers = []
+        if self.landmarks[self.tip_idx[0]][1] < self.landmarks[self.tip_idx[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        for idx in range(1,5):
+            if self.landmarks[self.tip_idx[idx]][2] < self.landmarks[self.tip_idx[idx] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        
+        return fingers
+                
 def main():
     cap = cv2.VideoCapture(0)
     detector = HandDetector()
